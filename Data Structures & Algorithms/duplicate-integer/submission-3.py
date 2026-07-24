45304aefe370d5c566a7f99917fc937f91036bf3
Nhien Loc Bui <15635@student.vgu.edu.vn>
2026-07-24 16:14:53 -0700

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        result = {}
+        for num in nums:
+            if num in result:
+                return True
+
+            result[num] = 1
+
+        return False
+
